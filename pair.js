2011f3495be6ebec07d001b88d518c1092ebffc1
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-05-18 11:29:00 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -11,7 +11,7 @@ const {
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("maher-zubair-baileys");
+} = require("@whiskeysockets/baileys"); // Updated Baileys version
 
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
